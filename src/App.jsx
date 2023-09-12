49f2602c00cf21bf01d4123f233736e87d935749
Forhad5194 <forhadhossain5194@gmail.com>
2023-09-12 12:23:03 +0600

    Json custom data load

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './componets/Blogs/Blogs'
+import Header from './componets/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
